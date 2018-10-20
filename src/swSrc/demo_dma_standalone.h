ad9a0ebf9ac1df3bf0e9a7e900e27dd06304f94e
gutelfuldead <gutel@hcs.ufl.edu>
2018-10-19 23:23:05 -0700

    removed xilinx provided demos and testing files to prevent confusion

--- src/swSrc/demo_dma_standalone.h
+++
@@ -1,6 +0,0 @@
-#ifndef DMA_SG_INTR_H_INC
-#define DMA_SG_INTR_H_INC
-
-int demo_dma_standalone(void);
-
-#endif // DMA_SG_INTR_H_INC
--- src/swSrc/dma_sg_poll.h
+++
@@ -1,6 +0,0 @@
-#ifndef DMA_SG_POLL_H_INC
-#define DMA_SG_POLL_H_INC
-
-int dma_sg_poll_exec(void);
-
-#endif // DMA_SG_POLL_H_INC
